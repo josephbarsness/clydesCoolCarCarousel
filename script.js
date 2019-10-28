60ac0d243d20581cee47bc33523af4a0984fad6a
Joey Barsness <joseph.barsness@snapav.com>
2019-10-28 14:40:51 -0500

    image reload button works

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-console.log( 'js' );
+console.log(' js ');
 
-function buttonClickThing(){
-    
+function buttonClickThing () {
+    var d = new Date();
+    document.getElementById("imageid").src = "https://source.unsplash.com/344x220/?car" + d.getTime();
+    console.log(' done ');
 }
