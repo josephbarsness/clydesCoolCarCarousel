815555f12952a4f40de96dec0604259760f32e2c
Joey Barsness <joseph.barsness@snapav.com>
2019-11-04 13:05:46 -0600

    kinda sorta working

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.log(' js ');
 
-function buttonClickThing () {
+function buttonClickThing(elem) {
     var d = new Date();
-    document.getElementById("imageid").src = "https://source.unsplash.com/344x220/?car" + d.getTime();
+
+    elem.closest('.carsButton').querySelector('img').src =
+        "https://source.unsplash.com/344x220/?cars" + d.getTime()
+
     console.log(' done ');
-}
+};
+
+
